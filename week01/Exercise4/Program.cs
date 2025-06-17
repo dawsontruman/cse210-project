b5ccfea39e8a4fe99d547d8a0c23b412f2b9fbad
Dawson Truman <dawsontruman2525@gmail.com>
2025-06-17 16:30:11 -0600

    Completed core requirements for Exercise 4: Lists and Generics

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<float> numbers = new List<float>();
+        float number = 0;
+
+        Console.WriteLine("Enter a list of nonzero numbers, enter 0 when finished.");
+        do
+        {
+            Console.Write("Enter a number: ");
+            number = float.Parse(Console.ReadLine());
+            numbers.Add(number);
+        } while (number != 0);
+
+        float sum = 0;
+        foreach (float n in numbers)
+        {
+            sum += n;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // find largest number in list, using the first number for initial comparison
+        float max = numbers[0];
+        foreach (float i in numbers)
+        {
+            if (i >= max)
+            {
+                max = i;
+            }
+        }
+        Console.WriteLine($"The maximum number is: {max}");
+
     }
 }
