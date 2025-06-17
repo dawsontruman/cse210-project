a43fffd66651199119c754ee1cedd2fd1778f3eb
Dawson Truman <dawsontruman2525@gmail.com>
2025-06-17 14:51:49 -0600

    Completed the core requirements for Exercise 2: If Statements

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?");
+        string userGrade = Console.ReadLine();
+        float grade = float.Parse(userGrade);
+
+        string letterGrade;
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        Console.WriteLine($"Your letter grade is: {letterGrade}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congratulations! You passed tha class!");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is not high enough to pass the class. Try harder next time!");
+        }
     }
 }
