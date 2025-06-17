6b07fa550a1794d432a3f172e49b5f38e9aed9e3
Dawson Truman <dawsontruman2525@gmail.com>
2025-06-17 15:23:37 -0600

    Completed core requirements for Exercise 3: Loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Generate magic number
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        // gameplay loop
+        int guess;
+        int guesses = 0;
+        do
+        {
+            guesses += 1;
+            Console.WriteLine("Guess a number.");
+            guess = int.Parse(Console.ReadLine());
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine($"Yes! The magic number is {number}.");
+                Console.WriteLine($"You got it in {guesses} guesses.");
+            }
+        } while (guess != number);
     }
 }
